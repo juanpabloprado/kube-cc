9c7db5fd20dcadf0cae0e8a4fcd8d4a52bbd5862
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2018-06-28 11:28:11 -0400

    externalizing config

--- src/main/java/io/igx/cloud/kubecc/controllers/InfoController.java
+++ src/main/java/io/igx/cloud/kubecc/controllers/InfoController.java
@@ -1,18 +1,25 @@
 package io.igx.cloud.kubecc.controllers;
 
 import org.cloudfoundry.client.v2.info.GetInfoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController extends V2Controller {
 
+    @Value("${token.endpoint}")
+    private String tokenEndpoint;
+
+    @Value("${auth.endpoint}")
+    private String authEndpoint;
+
     @GetMapping("/info")
     public GetInfoResponse info() {
         return GetInfoResponse.builder()
                 .apiVersion("2.114.0")
-                .tokenEndpoint("http://localhost:8080/uaa")
-                .authorizationEndpoint("http://localhost:8080/uaa")
+                .tokenEndpoint(tokenEndpoint)
+                .authorizationEndpoint(authEndpoint)
                 .build();
     }
 }
